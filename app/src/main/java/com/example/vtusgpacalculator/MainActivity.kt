7dc60f40f7a260494f236abc26f55f3a989abd75
harshitkarn <harshitsma2011@gmail.com>
2022-07-15 16:25:57 +0530

    2st commit

--- app/src/main/java/com/example/vtusgpacalculator/MainActivity.kt
+++ app/src/main/java/com/example/vtusgpacalculator/MainActivity.kt
@@ -1,26 +1,56 @@
 package com.example.vtusgpacalculator
 
+import android.app.Dialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        var buttons= listOf<TextView>(findViewById(R.id.button1),findViewById(R.id.button2),findViewById(R.id.button3),findViewById(R.id.button4),findViewById(R.id.button5),findViewById(R.id.button6),findViewById(R.id.button7),findViewById(R.id.button8))
-        for(i in 1..8){
-            buttons[i-1].setOnClickListener{
-                val message = "$i"
-                val intent = Intent(this, SGPAPage::class.java).apply {
-                    putExtra(EXTRA_MESSAGE, message)
+        var dialog: Dialog = Dialog(this)
+        var btn = findViewById<TextView>(R.id.calc_SGPA)
+        btn.setOnClickListener {
+            try {
+                dialog.setContentView(R.layout.select_dialog)
+                var buttons= listOf<TextView>(dialog.findViewById(R.id.sem1),dialog.findViewById(R.id.sem2),dialog.findViewById(R.id.sem3),dialog.findViewById(R.id.sem4),dialog.findViewById(R.id.sem5),dialog.findViewById(R.id.sem6),dialog.findViewById(R.id.sem7),dialog.findViewById(R.id.sem8))
+                for(i in 1..8){
+                    buttons[i-1].setOnClickListener{
+                        val message = "$i"
+                        val intent = Intent(this, SGPAPage::class.java).apply {
+                            putExtra(EXTRA_MESSAGE, message)
+                        }
+                        startActivity(intent)
+                    }
                 }
-                startActivity(intent)
+                val closeButton = dialog.findViewById<ImageView>(R.id.dialogclsa)
+                closeButton.setOnClickListener{
+                    dialog.dismiss()
+                }
+                dialog.show()
+            }
+            catch (e: Exception){
+                Toast.makeText(applicationContext,e.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+        val calcCgpa = findViewById<TextView>(R.id.calc_CGPA)
+        calcCgpa.setOnClickListener {
+            try{
+                val intentCGPA = Intent(this, CGPAPage::class.java)
+                startActivity(intentCGPA)
+            }
+            catch (e: Exception){
+                Toast.makeText(applicationContext,e.toString(),Toast.LENGTH_SHORT).show()
             }
         }
     }
 }
+
